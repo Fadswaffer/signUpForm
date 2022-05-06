74ef0ca86f74705fbdb4b6c8b61b7e8a19b6cae4
Fadswaffer <sacw90@gmail.com>
2022-05-06 11:32:26 -0500

    Push of mostly completed sign-up-form

---
+++ script.js
@@ -0,0 +1,54 @@
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+const errorMessage = document.querySelector(".errorMessage");
+
+const inputs = [email, phone, password, confirmPassword];
+
+inputs.forEach((item) => {
+  item.addEventListener("focusin", () => {
+    errorMessage.textContent = "";
+    item.classList.remove("error");
+    if (item == password || item == confirmPassword) {
+      password.classList.remove("error");
+      confirmPassword.classList.remove("error");
+    }
+  });
+});
+
+const submit = (e) => {
+  e.preventDefault();
+  if (password.value !== confirmPassword.value) {
+    password.classList.add("error");
+    confirmPassword.classList.add("error");
+    errorMessage.textContent = "Passwords don't match";
+    return;
+  }
+  if (
+    !phone.value.match(
+      /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/
+    )
+  ) {
+    phone.classList.add("error");
+    errorMessage.textContent = "Phone number needs to be exactly 10 numbers";
+    return;
+  }
+  if (password.value.length < 8) {
+    password.classList.add("error");
+    errorMessage.textContent = "Must be longer than 8 characters";
+    return;
+  }
+  if (!password.value.match(/[a-z]/)) {
+    password.classList.add("error");
+    errorMessage.textContent = "Must have at least one lowercase letter";
+    return;
+  }
+  if (!password.value.match(/[A-Z]/)) {
+    password.classList.add("error");
+    errorMessage.textContent = "Must have at least one lowercase letter";
+    return;
+  }
+};
+const form = document.querySelector("form");
+form.addEventListener("submit", submit);
